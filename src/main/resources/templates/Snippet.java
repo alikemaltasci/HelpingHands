ddb1201084816c56795f919c00c9565860df94f3
alikemaltasci@mac <alikemaltasci@yahooo.com>
2017-02-21 14:55:51 +0300

    deleted a wrong file

--- src/main/resources/templates/Snippet.java
+++
@@ -1,9 +0,0 @@
-package templates;
-
-public class Snippet {
-	Project project = new Project();
-			project.setUserId(1L);
-			project.setName("Refugee Support2");
-			project.setDescription("Refugee Support desc");
-}
-
